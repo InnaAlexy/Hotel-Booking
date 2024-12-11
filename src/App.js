3023dd4683d22a8d5ef10b0adf88e6e8366ae802
InnaAlexy <life.inna.am@yandex.ru>
2024-12-11 13:04:01 +0300

    Реализовать роутинг

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
+import { Route, Routes } from 'react-router-dom';
 import styled from 'styled-components';
 
-const Div = styled.div`
+const Content = styled.div`
 	text-align: center;
 `;
+
+export const Header = () => {
+	return <div></div>;
+};
+
+export const Footer = () => {
+	return <div></div>;
+};
+
 function App() {
 	return (
-		<Div>
-			<i className="fa fa-calendar"></i>
-			<div>hi</div>
-		</Div>
+		<>
+			<Header />
+			<Content>
+				<h2> Заголовок </h2>
+				<Routes>
+					<Route path="/" element={<div>Главная</div>}></Route>
+					<Route path="/login" element={<div>Вход</div>}></Route>
+					<Route path="/register" element={<div>Регистрация</div>}></Route>
+					<Route
+						path="/reserves"
+						element={<div>Список бронирований</div>}
+					></Route>
+					<Route path="/rooms" element={<div>Выбор номеров</div>}></Route>
+					<Route path="/room/:roomId" element={<div>Номер</div>}></Route>
+					<Route
+						path="/myBooking"
+						element={<div>Мои бронирования</div>}
+					></Route>
+					<Route path="*" element={<div>Ошибка</div>}></Route>
+				</Routes>
+			</Content>
+			<Footer />
+		</>
 	);
 }
 
